dcb2c4a0063b125a8dad3b303b2cbe0205a2433d
Joey <tiefejos@gmail.com>
2017-10-22 13:49:51 -0600

    Working oauth20 feature
    It works good enough that you can log in and log out.
    
    I'll work on config.json where it store client secret code and connect to mongo

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+
+
 var express = require('express');
 var path = require('path');
 //var favicon = require('serve-favicon');
@@ -5,17 +7,26 @@ var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
+
+//Routes
 var index = require('./routes/index');
 var data = require('./routes/data');
+var oauth2 = require('./lib/oauth2');
 
-var app = express();
 
+const passport = require('passport');
+const session = require('express-session');
+
+var GOOGLE_CLIENT_ID = process.env['40503956114-tpipggvtu7vau02q78roviac34gea6fs.apps.googleusercontent.com'];
+var GOOGLE_API_KEY = process.env['32'];
+
+
+const app = express();
+
+//Database
 const mongoose = require('mongoose');
-
 mongoose.connect('mongodb://localhost/cobra');
-
 var db = mongoose.connection;
-
 //Check connection
 db.once('open', function () {
     console.log('Connected to MongoDB')
@@ -28,24 +39,44 @@ db.on('error', function (err) {
 });
 
 
-// view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-
-// uncomment after placing your favicon in /public
-//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
-app.use(logger('dev'));
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use('/', index);
-app.use('/',data);
-
 //Using express.static middleware to server all public files
 //Note: the path that you provide to the express.static f(x) is relative to the directory from where you launch
 //your node process. Which is why you used path and dirname
 app.use(express.static(path.join(__dirname, 'public')));
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+app.use(logger('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+//OAuth
+const sessionConfig = {
+    resave: false,
+    saveUninitialized: false,
+    secret: "32",
+    signed: true
+};
+
+app.use(session(sessionConfig));
+
+// OAuth2
+app.use(passport.initialize());
+app.use(passport.session());
+app.use(require('./lib/oauth2').router);
+
+
+//Routes
+app.use('/', index);
+app.use('/',data);
+
+
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -66,9 +97,3 @@ app.use(function(err, req, res, next) {
 });
 
 module.exports = app;
-
-//
-// //creating a server that browsers can connect to
-// app.listen(3000, function () {
-//     console.log('listening on 3000')
-// });
