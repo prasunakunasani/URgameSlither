fa84aca910e2175b68b86635d4e4554e250f3860
Chris Boyd <boyd111c@gmail.com>
2017-10-30 22:01:43 -0600

    Moved slither server inside the app.

---
+++ config/config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    port: 8080,
+    bots: 0, // Not Implemented
+    maxConnections: 100,
+	highscoreName: "How to change Highscore Msg",
+	highscoreMsg: "Set this message in the config",
+    food: 1000,
+    foodColors: 8,
+    foodSize: [15, 47],
+    foodPerSpawn: 1000, // Not Implemented
+    gameRadius: 2000,//21600,
+    sectorSize: 300
+};
---
+++ src/messages/increase.js
@@ -0,0 +1,13 @@
+var message = require('../utils/message');
+
+var type = 'n'.charCodeAt(0);
+
+exports.build = function (snake) {
+    var arr = new Uint8Array(12);
+    message.writeInt8(2, arr, type);
+	  message.writeInt16(3, arr, snake.id);
+    message.writeInt16(5, arr, snake.body.x);
+    message.writeInt16(7, arr, snake.body.y);
+	  message.writeInt24(9, arr, snake.fam);
+    return arr;
+};
